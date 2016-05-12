f0ca7a0af46e6e0bac10f003f7452d7d2f585d24
Will Kraft <willkraft@comcast.net>
2016-05-12 13:00:29 -0400

    made code look nicer ur welcome

--- src/EndScreen.java
+++ src/EndScreen.java
@@ -1,28 +1,18 @@
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class EndScreen extends JPanel{
-	private JLabel title, score;
-	private JPanel panel;
+	private JLabel score;
 	private Window win;
 	private int points;
 	private JButton playAgain, mainMenu;
-	private String[] themeNames = {"Day", "Night"};
-	private Theme[] themes;
-	private JComboBox<String> themeBox;
-	private Theme currentTheme;
+	
 	public EndScreen(Window w){
 		win = w;
 		setSize(new Dimension(700, 400));
@@ -39,32 +29,13 @@ public class EndScreen extends JPanel{
 		add(score);
 		add(butts);
 		
-		//panel = new JPanel();
-	/*	setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		setBorder(BorderFactory.createEmptyBorder(50, 50, 50, 50));
-		title = new JLabel("Game Over");
-		title.setSize(100, 100);add(title);
-		setBackground(new Color(0,0,0,0));
-		//title.setAlignmentX(JLabel.CENTER_ALIGNMENT);
-		score = new JLabel("Score: ");
-		JButton button = new JButton("Play Again");
-		button.setActionCommand("Play Again");
-		
-		button.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent event) {
-				win.getGame().getCat().newCat();
-				win.startGame();
-			}
-		});
-		button.setAlignmentX(JPanel.CENTER_ALIGNMENT);
-		this.add(button);*/
 	}
-	
 	
 	public void setScore(int scr){
 		points = scr;
 		score.setText("Score: " + points);
 	}
+
 	private void setUp(){
 		score = new JLabel("Score: " + win.getGame().getScore());
 		score.setFont(new Font(Font.DIALOG, Font.PLAIN, 40));
@@ -89,13 +60,19 @@ public class EndScreen extends JPanel{
 				win.goToIntro();
 			}
 		});
+		
 	}
+	
 	public void update(){
 		score.setForeground(win.getSettings().getTheme().getFontColor());
+		
 	}
+	
 	public void paint(Graphics g){
 		Graphics2D g2 = (Graphics2D)g;
 		g2.drawImage(win.getSettings().getTheme().getSkyImage(), 0, -600, null, null);
 		super.paint(g);		
+		
 	}
+	
 }

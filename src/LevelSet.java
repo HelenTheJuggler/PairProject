ddb118b8469883f35fb5453a626e09b7b330bab6
Will Kraft <willkraft@comcast.net>
2016-05-17 13:17:39 -0400

    level 6 and 7

--- src/LevelSet.java
+++ src/LevelSet.java
@@ -5,7 +5,7 @@ public class LevelSet {
 	int current;
 	
 	public LevelSet(){
-		levs = new Level[5];
+		levs = new Level[7];
 		current = 0;
 		
 		levs[0] = new Level(null, new Goal("Bird"), null, 
@@ -26,6 +26,13 @@ public class LevelSet {
 		Obstacle[] obs2 = {new Obstacle(425, 150)};
 		Goal[] coins2 = {new Goal(new Point(450,75),"Fish")};
 		levs[4] = new Level(obs2, new Goal("Bird"), coins2, null, false);
+		
+		Obstacle[] obs3 = {new Obstacle(600,100), new Obstacle(600,200), new Obstacle(600, 300)};
+		Goal[] coins3 = {new Goal(new Point(380,100), "Fish")};
+		levs[5] = new Level(obs3, new Goal(new Point(550, 300),"Bird"), coins3, null, false);
+		
+		Goal[] coins4 = {new Goal(new Point(400, 180), "Fish"), new Goal(new Point(500, 200), "Fish"), new Goal(new Point(600, 220), "Fish"), new Goal(new Point(700, 230), "Fish")};
+		levs[6] = new Level(null, new Goal(new Point(750, 300),"Bird"), coins4, null, false);
 	}
 	
 	public void nextLevel(){
